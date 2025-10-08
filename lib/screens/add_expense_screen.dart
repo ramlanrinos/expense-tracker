6ded026c8b0685fcbdc8d3ef17a3a004b0f9baa6
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2025-10-08 19:17:35 +0530

    AddExpense form's TextField changed

--- lib/screens/add_expense_screen.dart
+++ lib/screens/add_expense_screen.dart
@@ -9,10 +9,13 @@ class AddExpenseScreen extends StatefulWidget {
 }
 
 class _AddExpenseScreenState extends State<AddExpenseScreen> {
-  final _titleController = TextEditingController();
-  final _amountController = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
   DateTime? _selectedDate;
 
+  // A key used to identify and manage the state of the Form widget in UI.
+  // _formKey.currentState?.validate() → to check if inputs are valid.
+  // _formKey.currentState?.reset() → to clear the form.
   final _formKey = GlobalKey<FormState>();
 
   void showDatepicker() async {
@@ -20,7 +23,7 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
       context: context,
       firstDate: DateTime(2020),
       lastDate: DateTime.now(),
-      initialDate: _selectedDate == null ? DateTime.now() : _selectedDate,
+      initialDate: _selectedDate ?? DateTime.now(),
     );
 
     if (pickedDate != null) {
@@ -47,7 +50,11 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
       //   "amount": amount,
       // };
 
-      final newExpense = ExpenseModel(title: title, date: _selectedDate, amount: amount);
+      final newExpense = ExpenseModel(
+        title: title,
+        date: _selectedDate,
+        amount: amount,
+      );
 
       Navigator.pop(context, newExpense);
     }
@@ -68,70 +75,85 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
       appBar: AppBar(title: Text("Add Expense")),
       body: Form(
         key: _formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: "Title"),
-              controller: _titleController,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return "Title is required";
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: "Amount"),
-              controller: _amountController,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return "Amount is required";
-                }
-                if (double.tryParse(value) == null) {
-                  return "Enter a number";
-                }
-                return null;
-              },
-            ),
-            Text(
-              _selectedDate == null
-                  ? "No date selected"
-                  : "Selected Date: ${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}",
-            ),
-            TextButton(
-              onPressed: () => showDatepicker(),
-              child: Text("Select Date"),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () => submitForm(),
-                  child: Text("Add Expense"),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.indigoAccent,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Title",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 2, style: BorderStyle.solid),
                   ),
                 ),
-                SizedBox(width: 15.0),
-                ElevatedButton(
-                  onPressed: () => resetForm(),
-                  child: Text("Rest"),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.redAccent,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
+                controller: _titleController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Title is required";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Amount",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 2, style: BorderStyle.solid),
                   ),
                 ),
-              ],
-            ),
-          ],
+                controller: _amountController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Amount is required";
+                  }
+                  if (double.tryParse(value) == null) {
+                    return "Enter a number";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              Text(
+                _selectedDate == null
+                    ? "No date selected"
+                    : "Selected Date: ${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}",
+              ),
+              TextButton(
+                onPressed: () => showDatepicker(),
+                child: Text("Select Date"),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => submitForm(),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.indigoAccent,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: Text("Add Expense"),
+                  ),
+                  SizedBox(width: 15.0),
+                  ElevatedButton(
+                    onPressed: () => resetForm(),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.redAccent,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    child: Text("Rest"),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
