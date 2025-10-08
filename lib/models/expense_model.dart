7f8a2ab510a03fb57f03a08bdd0d32e9a0e20f17
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2025-10-08 22:01:51 +0530

    Adaptor file created for ExpenseModel

--- lib/models/expense_model.dart
+++ lib/models/expense_model.dart
@@ -1,6 +1,14 @@
-class ExpenseModel {
+import 'package:hive/hive.dart';
+
+part 'expense_model.g.dart';
+
+@HiveType(typeId: 0)
+class ExpenseModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final DateTime? date;
+  @HiveField(2)
   final double amount;
 
   ExpenseModel({required this.title, required this.date, required this.amount});
